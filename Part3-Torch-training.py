f05158e2c4fcd1ce514af3d720e10c343a117277
Mohammad Mahdi Sangtarash <m.mahdi.sangtarash@gmail.com>
2024-03-19 00:04:51 +0330

    part 3 - PyTorch learning

---
+++ Part3-Torch-training.py
@@ -0,0 +1,16 @@
+import torch
+import numpy as np
+
+# Create a arange tensor
+
+arange_tensor = torch.arange(4, 15, 3)
+print(f"arange tensor: {arange_tensor}")
+print("==============================\n")
+
+
+# Create a linespace tensor
+
+linespace_tensor = torch.linspace(6, 17, 8)
+print(f"linespace tensor: {linespace_tensor}")
+print("==============================\n")
+
